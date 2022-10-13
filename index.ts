e4f1c438c68b0ebba4c18e76c079588089516fe3
Marcin Lewandowski <marcin@saepia.net>
2022-10-13 17:14:11 +0200

    Fixed export types

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import { configureLocalState } from './src/react/configure';
-import { LocalState, LocalStateMultiReadResult } from './src/LocalState';
+import { LocalState } from './src/LocalState';
+import { 
+  LocalStateMultiGetters,
+  LocalStateMultiSetters,
+  LocalStateMultiRemovers,
+  LocalStateMulti,
+ } from './src/types';
 import { LocalStateBackend } from './src/LocalStateBackend';
 import { LocalStateMemoryBackend } from './src/backends/LocalStateMemoryBackend';
 
 export { 
   configureLocalState,
   LocalState,
-  LocalStateMultiReadResult,
+  LocalStateMultiGetters,
+  LocalStateMultiSetters,
+  LocalStateMultiRemovers,
+  LocalStateMulti,
   LocalStateBackend, 
   LocalStateMemoryBackend,
  };
